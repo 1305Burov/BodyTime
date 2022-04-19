e292b19ed268892951b75c39dfcf987d69669113
1305Burov <1305burov@gmail.com>
2022-04-19 12:55:26 +0300

    section discount and swiper added

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,24 @@ import * as flsFunc from './moduls/func.js';
 
 flsFunc.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    
+});
